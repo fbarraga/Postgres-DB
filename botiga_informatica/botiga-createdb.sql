b649918acd50bfb5e2f554c16f60167c8da941a7
Francesc Barragán Pascual <francescbarragan@msn.com>
2022-10-09 18:11:00 +0200

    Alta fitxer

---
+++ botiga_informatica/botiga-createdb.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS tienda;
+CREATE DATABASE tienda;
+USE tienda;
---
+++ universitat/universitat-schema.sql
@@ -0,0 +1,63 @@
+
+ 
+CREATE TABLE departamento (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE persona (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nif VARCHAR(9) UNIQUE,
+    nombre VARCHAR(25) NOT NULL,
+    apellido1 VARCHAR(50) NOT NULL,
+    apellido2 VARCHAR(50),
+    ciudad VARCHAR(25) NOT NULL,
+    direccion VARCHAR(50) NOT NULL,
+    telefono VARCHAR(9),
+    fecha_nacimiento DATE NOT NULL,
+    sexo ENUM('H', 'M') NOT NULL,
+    tipo ENUM('profesor', 'alumno') NOT NULL
+);
+ 
+CREATE TABLE profesor (
+    id_profesor INT UNSIGNED PRIMARY KEY,
+    id_departamento INT UNSIGNED NOT NULL,
+    FOREIGN KEY (id_profesor) REFERENCES persona(id),
+    FOREIGN KEY (id_departamento) REFERENCES departamento(id)
+);
+ 
+ CREATE TABLE grado (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL
+);
+ 
+CREATE TABLE asignatura (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    creditos FLOAT UNSIGNED NOT NULL,
+    tipo ENUM('básica', 'obligatoria', 'optativa') NOT NULL,
+    curso TINYINT UNSIGNED NOT NULL,
+    cuatrimestre TINYINT UNSIGNED NOT NULL,
+    id_profesor INT UNSIGNED,
+    id_grado INT UNSIGNED NOT NULL,
+    FOREIGN KEY(id_profesor) REFERENCES profesor(id_profesor),
+    FOREIGN KEY(id_grado) REFERENCES grado(id)
+);
+ 
+CREATE TABLE curso_escolar (
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    anyo_inicio YEAR NOT NULL,
+    anyo_fin YEAR NOT NULL
+);
+
+CREATE TABLE alumno_se_matricula_asignatura (
+    id_alumno INT UNSIGNED NOT NULL,
+    id_asignatura INT UNSIGNED NOT NULL,
+    id_curso_escolar INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id_alumno, id_asignatura, id_curso_escolar),
+    FOREIGN KEY (id_alumno) REFERENCES persona(id),
+    FOREIGN KEY (id_asignatura) REFERENCES asignatura(id),
+    FOREIGN KEY (id_curso_escolar) REFERENCES curso_escolar(id)
+);
+ 
+ 
